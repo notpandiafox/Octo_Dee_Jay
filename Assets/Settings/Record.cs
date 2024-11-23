ce1593a37bc8d6f63edf4ef96c7f89c56ae50f75
notpandiafox <105837673+notpandiafox@users.noreply.github.com>
2024-11-23 13:05:22 -0800

    add project files

---
+++ Assets/Settings/Record.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Record
+{
+    private float timePassing;
+    private AudioSource song;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
