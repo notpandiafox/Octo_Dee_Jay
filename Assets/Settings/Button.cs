98cdd1916996e02478ae3f8678d217c3cf989651
terekj <30293049+terekj@users.noreply.github.com>
2024-11-23 17:56:16 -0800

    Add button, soundboard and audio files

--- Assets/Settings/Button.cs
+++ Assets/Settings/Button.cs
@@ -1,25 +1,33 @@
 using System.Collections;
 using System.Data;
+using System.Runtime.InteropServices;
+using Unity.VisualScripting;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class Button : MonoBehaviour
 {
-    private const float MOVE_DISTANCE = 3f;
-    private const float MOVE_DURATION = 0.1f;
+    private const float MOVE_DISTANCE = 0.06f;
+    private const float MOVE_DURATION = 0.2f;
     private GameObject m_GameObject;
     private Sprite m_OnSprite;
     private Sprite m_OffSprite;
     private AudioClip m_Track;
     private AudioSource m_AudioSource;
 
-    public Button(GameObject buttonObject, Sprite onSprite, Sprite offSprite, AudioClip track) {
+    private bool m_CanPlay;
+
+    public void initialize(GameObject buttonObject, Sprite onSprite, Sprite offSprite, AudioClip track) {
         m_GameObject = buttonObject;
 
-        m_AudioSource = new AudioSource();
+        m_GameObject.AddComponent<AudioSource>();
+        m_AudioSource = m_GameObject.GetComponent<AudioSource>();
         m_AudioSource.clip = track;
 
         m_OnSprite = onSprite;
         m_OffSprite = offSprite;
+
+        m_CanPlay = true;
     }
 
     public IEnumerator playDownAnimation() {
@@ -29,14 +37,20 @@ public class Button : MonoBehaviour
         );
         m_GameObject.GetComponent<SpriteRenderer>().sprite = m_OnSprite;
         yield return new WaitForSeconds(MOVE_DURATION);
-    }
-    public void playEffect() {
-        m_AudioSource.Play();
-        StartCoroutine(playDownAnimation());
         m_GameObject.transform.position = new Vector3(
             m_GameObject.transform.position.x,
             m_GameObject.transform.position.y + MOVE_DISTANCE
         );
         m_GameObject.GetComponent<SpriteRenderer>().sprite = m_OffSprite;
+    }
+    public void playEffect() {
+        if (m_CanPlay) {
+            m_CanPlay = false;
+            m_AudioSource.Play();
+            StartCoroutine(playDownAnimation());
+        }
     } 
+    public void free() {
+        m_CanPlay = true;
+    }
 }
---
+++ Assets/Settings/main.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class main : MonoBehaviour
+{
+    private GameObject m_GameObject;
+    private AudioSource m_AudioSource;
+    private AudioClip m_AudioClip;
+
+    private SoundBoard m_SoundBoard;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        m_GameObject = new GameObject("m_Game Object");
+        m_GameObject.AddComponent<AudioSource>();
+        m_AudioSource = m_GameObject.GetComponent<AudioSource>();
+        m_AudioClip = Resources.Load<AudioClip>("audio/MAIN THEME");
+
+        m_AudioSource.loop = true;
+        m_AudioSource.clip = m_AudioClip;
+        m_AudioSource.Play();
+
+        Dictionary<KeyCode, Button> buttons = new Dictionary<KeyCode, Button>();
+
+        Button U_Button = m_GameObject.AddComponent<Button>();
+        U_Button.initialize(
+            GameObject.FindGameObjectWithTag("U_BUTTON"),
+            Resources.Load<Sprite>("sprites/buttons/U_ON"),
+            Resources.Load<Sprite>("sprites/buttons/U_OFF"),
+            Resources.Load<AudioClip>("audio/HI-HAT")
+        );
+        buttons.Add(KeyCode.U, U_Button);
+
+        Button I_Button = m_GameObject.AddComponent<Button>();
+        I_Button.initialize(
+            GameObject.FindGameObjectWithTag("I_BUTTON"),
+            Resources.Load<Sprite>("sprites/buttons/I_ON"),
+            Resources.Load<Sprite>("sprites/buttons/I_OFF"),
+            Resources.Load<AudioClip>("audio/SNARE")
+        );
+        buttons.Add(KeyCode.I, I_Button);
+
+        Button O_Button = m_GameObject.AddComponent<Button>();
+        O_Button.initialize(
+            GameObject.FindGameObjectWithTag("O_BUTTON"),
+            Resources.Load<Sprite>("sprites/buttons/O_ON"),
+            Resources.Load<Sprite>("sprites/buttons/O_OFF"),
+            Resources.Load<AudioClip>("audio/START BUTTON")
+        );
+        buttons.Add(KeyCode.O, O_Button);
+
+        Button J_Button = m_GameObject.AddComponent<Button>();
+        J_Button.initialize(
+            GameObject.FindGameObjectWithTag("J_BUTTON"),
+            Resources.Load<Sprite>("sprites/buttons/J_ON"),
+            Resources.Load<Sprite>("sprites/buttons/J_OFF"),
+            Resources.Load<AudioClip>("audio/RIM")
+        );
+        buttons.Add(KeyCode.J, J_Button);
+
+        Button K_Button = m_GameObject.AddComponent<Button>();
+        K_Button.initialize(
+            GameObject.FindGameObjectWithTag("K_BUTTON"),
+            Resources.Load<Sprite>("sprites/buttons/K_ON"),
+            Resources.Load<Sprite>("sprites/buttons/K_OFF"),
+            Resources.Load<AudioClip>("audio/KICK")
+        );
+        buttons.Add(KeyCode.K, K_Button);
+
+        Button L_Button = m_GameObject.AddComponent<Button>();
+        L_Button.initialize(
+            GameObject.FindGameObjectWithTag("L_BUTTON"),
+            Resources.Load<Sprite>("sprites/buttons/L_ON"),
+            Resources.Load<Sprite>("sprites/buttons/L_OFF"),
+            Resources.Load<AudioClip>("audio/START BUTTON")
+        );
+        buttons.Add(KeyCode.L, L_Button);
+
+        m_SoundBoard = new SoundBoard(buttons);
+    }
+    void OnGUI() {
+        Event currentEvent = Event.current;
+        if (currentEvent.isKey) {
+            if (currentEvent.keyCode == KeyCode.U || 
+                currentEvent.keyCode == KeyCode.I ||
+                currentEvent.keyCode == KeyCode.O ||
+                currentEvent.keyCode == KeyCode.J ||
+                currentEvent.keyCode == KeyCode.K ||
+                currentEvent.keyCode == KeyCode.L) {
+                    m_SoundBoard.triggerButton(currentEvent.keyCode);
+                    StartCoroutine(addDelay(currentEvent.keyCode));
+            } 
+        }
+    }
+    public IEnumerator addDelay(KeyCode key) {
+        yield return new WaitForSecondsRealtime(0.2f);
+        m_SoundBoard.freeButton(key);
+    }
+    void Update()
+    {
+    }
+}
+
